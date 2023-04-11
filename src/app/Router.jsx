44db50a65420ae0821bcb59d6ec0134d3d4fa38b
sonitiia <sofiatymechko@gmail.com>
2023-04-11 14:04:44 +0300

    add NotFoundRoute

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -1,17 +1,33 @@
 /** @format */
 
-import { Route, Routes } from "react-router-dom";
-import { DEFAULT_ROUTE, FULL_WIDTH_ROUTE } from "./Routes";
+import { Navigate, Route, Routes } from "react-router-dom";
+import {
+	DEFAULT_ROUTE,
+	FULL_WIDTH_ROUTE,
+	PAGE_NOT_FOUND_ROUTE,
+} from "./Routes";
 import DefaultLayout from "../layouts/DefaultLayout";
 import FullWidthLayout from "../layouts/FullWidthLayout";
+import NotFoundRoute from "../routes/NotFoundRoute";
 
 const Router = () => {
 	// prettier-ignore
 
 	return (
 		<Routes>
-			<Route path={DEFAULT_ROUTE} element={<DefaultLayout/>}></Route>
-			<Route path={FULL_WIDTH_ROUTE} element={<FullWidthLayout />}></Route>
+			<Route path={DEFAULT_ROUTE} element={<DefaultLayout />}>
+				<Route
+					path={PAGE_NOT_FOUND_ROUTE}
+					element={<NotFoundRoute />}
+				/>
+				<Route
+					path="*"
+					element={<Navigate to={PAGE_NOT_FOUND_ROUTE} replace />}
+				/>
+			</Route>
+			<Route
+				path={FULL_WIDTH_ROUTE}
+				element={<FullWidthLayout />}></Route>
 		</Routes>
 	);
 };
